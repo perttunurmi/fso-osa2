a143ad90d288a7f1a4829ce2e5fdb35c7c567876
perttunurmi <perttu.nurmi@gmail.com>
2024-11-30 20:51:58 +0200

    Notes part6 Käytetty noin 9h

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes.js'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -8,12 +9,10 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
   }, [])
 
@@ -32,11 +31,28 @@ const App = () => {
       important: Math.random() > 0.5,
     }
 
-    axios
-      .post("http://localhost:3001/notes", noteObject)
-      .then(response => {
-        setNotes(notes.concat(noteObject))
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
+      })
+  }
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        aler(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
       })
   }
 
@@ -55,7 +71,7 @@ const App = () => {
 
       <ul>
         {notesToShow.map(note => // map on kuvaus
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
         )}
       </ul>
       <form onSubmit={addNote}>
@@ -68,6 +84,5 @@ const App = () => {
     </div>
   )
 }
-
 
 export default App
