09736e3a97b014b2adfb7e2289d3000e9ab917b3
perttunurmi <perttu.nurmi@gmail.com>
2024-11-30 19:58:39 +0200

    notes part5

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -30,11 +30,14 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() > 0.5,
-      id: String(notes.length + 1),
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+      .post("http://localhost:3001/notes", noteObject)
+      .then(response => {
+        setNotes(notes.concat(noteObject))
+        setNewNote('')
+      })
   }
 
   const notesToShow = showAll
