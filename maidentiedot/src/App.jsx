009968c73b79051ff2a9c3f4faff31f1076509af
perttunurmi <perttu.nurmi@gmail.com>
2024-12-06 20:21:34 +0200

    Maidentiedot

--- maidentiedot/src/App.jsx
+++ maidentiedot/src/App.jsx
@@ -10,14 +10,47 @@ const Find = ({ value, handleChange, text }) => {
   )
 }
 
-const Countries = ({ countries }) => {
-  if (countries.length > 0) {
-    return <div> Too many matches, specify another filter </div>
-  }
+const Kielet = ({ kielet }) => {
+  const lista = Object.keys(kielet).map(kieli =>
+    <li key={kieli}>{kielet[kieli]}</li>
+  )
+
   return (
-    countries.map(() => {
-      <div> {countries[0].name.common} </div>
-    }
+    <ul>
+      {lista}
+    </ul>
+  )
+}
+
+const Countries = ({ countries, onChange }) => {
+  if (countries.length > 10) {
+    return (
+      <div>
+        Too many matches, specify another filter
+      </div>
+    )
+  }
+
+  if (countries.length > 1) {
+    return (
+      countries.map(country =>
+        <div key={country.name.common} onClick={onChange}>
+          {country.name.common} <button>show</button >
+        </div>
+      )
+    )
+  }
+
+  return (
+    countries.map(country =>
+      <div key={country.name.common}>
+        <h2>{country.name.common}</h2>
+        <div>capital {country.capital}</div>
+        <div>area {country.area}</div>
+        <h3>languages:</h3>
+        <Kielet kielet={country.languages} />
+        <img src={country.flags.svg} alt={`${country.name.common} lippu`} width={"150px"} />
+      </div>
     )
   )
 }
@@ -37,20 +70,21 @@ function App() {
 
   if (!countries) { return null; }
 
-
-  console.log(countries.filter(country => country.name.common.includes(filter)))
-
   const handleFilter = (event) => {
     const newFilter = event.target.value
     setFilter(newFilter)
   }
 
-  const FilteredCountries = countries.filter(country => country.name.common.includes(filter))
+  const showNew = (e) => {
+    console.log(e)
+  }
+
+  const FilteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
 
   return (
     <>
       <Find value={filter} handleChange={handleFilter} text="find countries" />
-      <Countries countries={FilteredCountries} />
+      <Countries countries={FilteredCountries} onChange={showNew} />
     </>
   )
 }
